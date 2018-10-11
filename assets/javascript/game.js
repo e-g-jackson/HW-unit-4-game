2965efb0b9db6d46586aa9aec385c04734287a4b
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2018-10-11 06:49:23 -0500

    Add files via upload
    Characters are finally moving!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
 //character variables defined
 var batman = {name: "batman", life: 100, attack: 25, img: "assets/images/batman.jpg"};
 var nightwing = {name: "nightwing", life: 100, attack: 25, img: "assets/images/nightwing.jpg"};
@@ -10,13 +9,14 @@ var gDiv = document.getElementById("gameDiv");
 var def = 0;
 var att = 0;
 console.log("variables defined");
+
 var defDiv = $('<div></div>');
     $(defDiv).attr('id', 'defDiv');
-    $(defDiv).html('Defender');
+    $(defDiv).html('<h2>Defender</h2>');
     $(gDiv).before(defDiv);
 var attDiv = $('<div></div>');
     $(attDiv).attr('id', 'attDiv');
-    $(attDiv).html('Attacker')
+    $(attDiv).html('<h2>Attacker</h2>')
     $(gDiv).after(attDiv);
 
 for ( let i = 0; i < char.length; i++){
@@ -24,39 +24,36 @@ for ( let i = 0; i < char.length; i++){
         $(charImg).attr('id', char[i].name);
         $(charImg).attr('src', char[i].img);
         $(charImg).attr('class', 'charImg');
+
     var charDiv = $('<div></div>');
         $(charDiv).attr('id', char[i].name + "_div");
         $(charDiv).append(charImg);
-        $(gDiv).append(charDiv);
+        $(charDiv).appendTo(gDiv);
             
     console.log(char[i].name + " loaded!");
 
 // end of for loop
 }
+
 $(document.getElementsByClassName('charImg')).on("click", function(){
     var picId = $(this).attr('id');
-    var picStr = ( '"#' + picId + '_div"' )
-    var pic = document.getElementById( '"#' + picId + '_div"' );
-        if( def == 0 ){
-            var def = 1;
-            $(pic).detatch();
-            $(defDiv).append(pic);
-            console.log('There is now a defender!')
+    var picStr = ( '"#' + picId + '_div"' );
+        if( def === 0 ){
+            def++;
+            $(this).detach().appendTo('#defDiv');
+            console.log('There is now a defender!');
         }
-        else if( def == 1 && att == 0){
-            var att = 1;
-            $(pic).detatch().appendTo(attDiv)
-            console.log('There is now an attacker!')
+        else if( def === 1 && att === 0){
+            att++;
+            $(this).detach().appendTo('#attDiv');
+            console.log('There is now an attacker!');
         }
-        else if ( def == 1 && att == 1){
-            alert('There is already an Attacker and a Defender!')
-            console.log('No more room!')
+        else if ( def === 1 && att === 1){
+            alert('There is already an Attacker and a Defender!');
+            console.log('No more room!');
         }
     console.log(picStr);
     });
-
-// end of ready fxn
-});
 
 // function to tell if there's an defender yet
 //on click call defender function and move accordingly
